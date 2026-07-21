613437fb81db8daea084c4a6f028784163880228
Azizbek <azizbekreimbaev1003@gmail.com>
2026-07-21 14:17:23 +0900

    feat: developed ProductService business logic to retreive data from backend

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -8,35 +8,48 @@ import Events from "./Events"
 //@ts-ignore
 import "../../../css/home.css"
 
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { Dispatch } from '@reduxjs/toolkit'
-import { createSelector } from 'reselect'
 import { setPopularDishes } from './slice'
-import { retrievePopularDishes } from './selector'
 import { Product } from "../../../lib/types/product";
+import ProductService from "../../services/ProductService";
+import { ProductCollection } from "../../../lib/enums/product.enum";
 
 /** REDUX SLICE and SELECTOR */
 const actionDispatch = (dispatch: Dispatch) => ({
     setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
 })
 
-const popularDishesRetriever = createSelector(retrievePopularDishes, (popularDishes) => ({ popularDishes }))
 
 export default function HomePage() {
     const { setPopularDishes } = actionDispatch(useDispatch())
-    const { popularDishes } = useSelector(popularDishesRetriever)
     // Selector: Store => Data
 
 
     useEffect(() => {
         // BACKEND server data request => Data
+        const product = new ProductService();
+
+        product.getProducts({
+            page: 1,
+            limit: 4,
+            order: "productViews",
+            productCollection: ProductCollection.DISH
+        })
+            .then(
+                (data) => {
+                    console.log("data pssed here", data)
+                    setPopularDishes(data)
+                }
+            )
+            .catch((err) => { console.log(err) })
 
         // Slice: Data => Store
-        // @ts-ignore
 
     }, [])
 
-    console.log(process.env.REACT_APP_API_URL)
+    // console.log(process.env.REACT_APP_API_URL)
+    
 
     return <div className={"homepage"} >
         <Statistics />
