4e93329796a263fec8f16a493719482bdd770e26
Azizbek <azizbekreimbaev1003@gmail.com>
2026-07-21 15:31:55 +0900

    feat: retrieve topUsers and modify ActiveUsers components

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -10,20 +10,23 @@ import "../../../css/home.css"
 
 import { useDispatch } from 'react-redux'
 import { Dispatch } from '@reduxjs/toolkit'
-import { setNewDishes, setPopularDishes } from './slice'
+import { setNewDishes, setPopularDishes, setTopUsers } from './slice'
 import { Product } from "../../../lib/types/product";
 import ProductService from "../../services/ProductService";
 import { ProductCollection } from "../../../lib/enums/product.enum";
+import MemberService from "../../services/MemberService";
+import { Member } from "../../../lib/types/member";
 
 /** REDUX SLICE and SELECTOR */
 const actionDispatch = (dispatch: Dispatch) => ({
     setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
     setNewDishes: (data: Product[]) => dispatch(setNewDishes(data)),
+    setTopUsers: (data: Member[]) => dispatch(setTopUsers(data))
 })
 
 
 export default function HomePage() {
-    const { setPopularDishes, setNewDishes } = actionDispatch(useDispatch())
+    const { setPopularDishes, setNewDishes, setTopUsers } = actionDispatch(useDispatch())
     // Selector: Store => Data
 
 
@@ -61,6 +64,17 @@ export default function HomePage() {
                 }
             )
             .catch((err) => { console.log(err) })
+
+
+        //MEMBER
+
+        const member = new MemberService();
+
+        member.getTopUsers()
+            .then((data) => {
+                setTopUsers(data)
+            })
+            .catch((err) => console.log(err))
 
 
     }, [])
