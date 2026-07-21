b21cbf11b01c238784c8e115520402b516e4f72f
Azizbek <azizbekreimbaev1003@gmail.com>
2026-07-21 14:56:22 +0900

    fix: developed also newDishes logic to retrieve data from backend

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -10,7 +10,7 @@ import "../../../css/home.css"
 
 import { useDispatch } from 'react-redux'
 import { Dispatch } from '@reduxjs/toolkit'
-import { setPopularDishes } from './slice'
+import { setNewDishes, setPopularDishes } from './slice'
 import { Product } from "../../../lib/types/product";
 import ProductService from "../../services/ProductService";
 import { ProductCollection } from "../../../lib/enums/product.enum";
@@ -18,11 +18,12 @@ import { ProductCollection } from "../../../lib/enums/product.enum";
 /** REDUX SLICE and SELECTOR */
 const actionDispatch = (dispatch: Dispatch) => ({
     setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
+    setNewDishes: (data: Product[]) => dispatch(setNewDishes(data)),
 })
 
 
 export default function HomePage() {
-    const { setPopularDishes } = actionDispatch(useDispatch())
+    const { setPopularDishes, setNewDishes } = actionDispatch(useDispatch())
     // Selector: Store => Data
 
 
@@ -42,14 +43,30 @@ export default function HomePage() {
                     setPopularDishes(data)
                 }
             )
+            .catch((err) => { console.log(err) });
+
+
+
+
+        product.getProducts({
+            page: 1,
+            limit: 4,
+            order: "createdAt",
+            productCollection: ProductCollection.DISH
+        })
+            .then(
+                (data) => {
+                    console.log("data pssed here newDishes", data)
+                    setNewDishes(data)
+                }
+            )
             .catch((err) => { console.log(err) })
 
-        // Slice: Data => Store
 
     }, [])
 
     // console.log(process.env.REACT_APP_API_URL)
-    
+
 
     return <div className={"homepage"} >
         <Statistics />
