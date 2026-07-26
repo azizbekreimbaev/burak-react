3d73aa045abc41ec6aed56f6b0412af1b7d4badf
Azizbek <azizbekreimbaev1003@gmail.com>
2026-07-26 22:01:10 +0900

    fix: modified BAsket logic with customized hooks

--- src/app/components/headers/OtherNavbar.tsx
+++ src/app/components/headers/OtherNavbar.tsx
@@ -5,11 +5,16 @@ import { CartItem } from "../../../lib/types/search";
 
 interface OtherNavbarProps {
     cartItems: CartItem[];
+    onAdd: (item: CartItem) => void
+    onRemove: (item: CartItem) => void
+    onDelete: (item: CartItem) => void
+    onDeleteAll: () => void
+
 }
 
 
 export default function OtherNavbar(props: OtherNavbarProps) {
-    const { cartItems } = props;
+    const { cartItems, onAdd, onRemove, onDelete, onDeleteAll } = props;
     const authUser = null
     return (
         <div className="other-navbar">
@@ -45,7 +50,11 @@ export default function OtherNavbar(props: OtherNavbarProps) {
 
                         {/* BASKET */}
 
-                        <Basket cartItems={cartItems} />
+                        <Basket cartItems={cartItems}
+                            onAdd={onAdd}
+                            onRemove={onRemove}
+                            onDelete={onDelete}
+                            onDeleteAll={onDeleteAll} />
 
                         {!authUser ? (
                             <Box>
