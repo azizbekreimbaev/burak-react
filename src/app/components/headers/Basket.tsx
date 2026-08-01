91a979fbee8555e8cc8830d0973935d7e10ade60
Azizbek <azizbekreimbaev1003@gmail.com>
2026-08-01 14:25:52 +0900

    feat: create develop createOrder and retrive data

--- src/app/components/headers/Basket.tsx
+++ src/app/components/headers/Basket.tsx
@@ -8,7 +8,10 @@ import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import { useHistory } from "react-router-dom";
 import { CartItem } from "../../../lib/types/search";
-import { serverApi } from "../../../lib/config";
+import { Messages, serverApi } from "../../../lib/config";
+import { sweetErrorHandling } from "../../../lib/sweetAlert";
+import { useGlobals } from "../../hooks/useGlobals";
+import OrderService from "../../services/OrderService";
 
 interface BAsketProps {
   cartItems: CartItem[];
@@ -20,7 +23,7 @@ interface BAsketProps {
 
 export default function Basket(props: BAsketProps) {
   const { cartItems, onAdd, onRemove, onDelete, onDeleteAll } = props
-  const authMember = null;
+  const { authMember } = useGlobals();
   const history = useHistory();
 
   const itemsPrice = cartItems.reduce((a: number, c: CartItem) => {
@@ -40,9 +43,34 @@ export default function Basket(props: BAsketProps) {
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(e.currentTarget);
   };
+
+
   const handleClose = () => {
     setAnchorEl(null);
   };
+
+  const proceedOrderHandler = async () => {
+    try {
+      handleClose();
+      if (!authMember) throw new Error(Messages.error2)
+
+      const order = new OrderService();
+
+      await order.createOrder(cartItems)
+
+      onDeleteAll();
+
+
+      //REFRESH WITH CONTEXT
+
+      history.push("/orders")
+
+    } catch (err) {
+      console.log(err)
+      sweetErrorHandling(err).then()
+    }
+  }
+
 
   return (
     <Box className={"hover-line"}>
@@ -139,7 +167,7 @@ export default function Basket(props: BAsketProps) {
           {cartItems.length !== 0 ? (
             <Box className={"basket-order"}>
               <span className={"price"}>Total: ${totalPrice} ({itemsPrice} + {shippingCost})</span>
-              <Button startIcon={<ShoppingCartIcon />} variant={"contained"}>
+              <Button startIcon={<ShoppingCartIcon />} variant={"contained"} onClick={proceedOrderHandler}>
                 Order
               </Button>
             </Box>
--- src/app/screens/ordersPage/index.tsx
+++ src/app/screens/ordersPage/index.tsx
@@ -1,4 +1,4 @@
-import { useState, SyntheticEvent } from "react";
+import { useState, SyntheticEvent, useEffect } from "react";
 import { Container, Stack, Box } from "@mui/material";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
@@ -10,9 +10,11 @@ import FinishedOrders from "./FinishedOrders";
 //@ts-ignore
 import "../../../css/order.css";
 import { Dispatch } from "@reduxjs/toolkit";
-import { Order } from "../../../lib/types/order";
+import { Order, OrderInquiry } from "../../../lib/types/order";
 import { setFinishedOrders, setPausedOrders, setProcessOrders } from "./slice";
 import { useDispatch } from "react-redux";
+import { OrderStatus } from "../../../lib/enums/order.enum";
+import OrderService from "../../services/OrderService";
 
 
 
@@ -31,9 +33,31 @@ export default function OrdersPage() {
 
   const [value, setValue] = useState("1");
 
- /**HANDLERS */
+  const [orderInquiry, setOrderInquiry] = useState<OrderInquiry>({
+    page: 1,
+    limit: 5,
+    orderStatus: OrderStatus.PAUSE
+  })
 
- 
+  useEffect(() => {
+    const order = new OrderService();
+
+    order.getMyOrders({ ...orderInquiry, orderStatus: OrderStatus.PAUSE })
+      .then((data) => { setPausedOrders(data) })
+      .catch((err) => { console.log(err) })
+
+    order.getMyOrders({ ...orderInquiry, orderStatus: OrderStatus.PROCESS })
+      .then((data) => { setProcessOrders(data) })
+      .catch((err) => { console.log(err) })
+
+    order.getMyOrders({ ...orderInquiry, orderStatus: OrderStatus.FINISH })
+      .then((data) => { setFinishedOrders(data) })
+      .catch((err) => { console.log(err) })
+
+  }, [orderInquiry])
+
+
+  /**HANDLERS */
 
   const handleChange = (e: SyntheticEvent, newValue: string) => {
     setValue(newValue);
